96261ec4f53cbecc8cac35fdb293e857fec19ac4
Gavin Bates <117932849+batesgavin9@users.noreply.github.com>
2022-11-30 17:02:55 +0000

    before day 3 - before row of buttons

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 import requests
-
+from discord.ui import Button,View
 
 intents = discord.Intents.all()
 
@@ -18,9 +18,39 @@ async def on_connect():
 
 
 @bot.command(brief="play tic tac toe using an API!")
-async def TTT(ctx,state,player):
+async def TTT(ctx):
   
   
+  global turn
+  global gameover
+  global count
+
+  global board
+  button1=Button(label = " ")
+  button2=Button()
+  button3=Button()
+  button4=Button()
+  button5=Button()
+  button6=Button()
+  button7=Button()
+  button8=Button()
+  button9=Button()
+
+
+  async def button1Clicked(interaction):
+    await interaction.response.send_message("thanks ")
+  
+  button1.callback=button1Clicked
+  view=View()
+  
+  view.add_item(button1)
+  
+
+  
+  
+
+  state="1"
+  player="1"
   
   url = "https://stujo-tic-tac-toe-stujo-v1.p.rapidapi.com/"+state+"/"+player
   my_secretAPI = os.environ['TTT']
@@ -31,7 +61,7 @@ async def TTT(ctx,state,player):
   
   response = requests.request("GET", url, headers=headers)
   
-  await ctx.send(response.text)
+  await ctx.send("hello",view=view)
 
 
 

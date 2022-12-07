8fb03f141b7facc1229a1f3803aa16f563ca42ad
Gavin Bates <117932849+batesgavin9@users.noreply.github.com>
2022-12-07 17:06:51 +0000

    pre thurs

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ async def on_connect():
 
 @bot.command(brief="play tic tac toe using an API!")
 async def TTT(ctx):
-  
+ 
   global id
+  #global rowforbutton
+  rowforbutton=3
   global turn
   global gameover
   global count
@@ -42,93 +44,162 @@ async def TTT(ctx):
   buttons=[button1, button2, button3, button4, button5, button6, button7, button8, button9] #made the buttons into list
  
   async def buttonClick():
-    
     #this code lets us know which button was clicked 1 through 9
     global id
+    nonlocal rowforbutton
     global buttons
     global turn
-    turn=""
+    turn="---------"
     nonlocal totalPlays
     totalPlays+=1
+
+    turnLetter=""
     #This alternates X and O - I thought we would need this but we dont
-    #if totalPlays%2==0:
+    if totalPlays%2==0:
+      turnLetter="X"
+    else: 
+      turnLetter="O"
     #buttons[int(id)]=Button(label = "O")
     #view=View()
     #for b in buttons:
-      #view.add_item(b)  
+      #view.add_item(b)  
     #else:
     #buttons[int(id)]=Button(label = "X")
-    #view=View() 
+    #view=View()
     #for b in buttons:
-      #view.add_item(b)  
+      #view.add_item(b)  
 
-    
+   
     print("Function ran"+ id)
     print(buttons[int(id)]) #gets correctly clicked button from list using the id
-    buttons[int(id)]=Button(label = "X")
+    buttons[int(id)]=Button(label = turnLetter, row=rowforbutton, style=discord.ButtonStyle.red)
+    buttons[int(id)].disabled=True
     view=View()
+    count=+1
+    turn+="X"             #according to console it added 1 to count variable
+    print(count)
+
+
+
+  #-----------------------------------------------------
+  #This works in trinket havent been able to test because phone dont work
+  #now we just need to change it so it adds the O
+  #I added back the alternatnig x and o
+
+    turn="---------"
+    
+
+    for i in range(len(turn)):
+      if i==id:
+        if totalPlays%2==0:
+          turn+="X"
+        else:
+          turn+="O"
+      else:
+        turn+=turn[i:i+1]
+
+    turn = turn[-9:]
+
+
+
+    print(turn)
+
+
+
+  #IF DOESNT WORK
+  #THIS WILL FS WORK-
+
+
+    #turn="---------"
+    #id=8
+
+    #for i in range(len(turn)):
+      #if i==id:
+        #turn+="X"
+      #else:
+        #turn+=turn[i:i+1]
+
+    #turn = turn[-9:]
+
+
+
+    #print(turn)
+
+
+
+
+
+
+
+
+
+
+
+    
+    
     for b in buttons:
       view.add_item(b)  
- 
+   
    
 
     await ctx.send("hello1",view=view)
-    
-    if button1==Button(label="X"):
-      count+1
-      turn+="X"
-    elif button1==Button(label="O"):
-      turn+="O"
-    elif button1!=Button(label="O")&button1!=Button(label="X"):
-      turn+="-"
-    if button2==Button(label="X"):
-      turn+="X"
-    elif button2==Button(label="O"):
-      turn+="O"
-    elif button2!=Button(label="O")&button2!=Button(label="X"):
-      turn+="-"
-    if button3==Button(label="X"):
-      turn+="X"
-    elif button3==Button(label="O"):
-      turn+="O"
-    elif button3!=Button(label="O")&button3!=Button(label="X"):
-      turn+="-"
-    if button4==Button(label="X"):
-      turn+="X"
-    elif button4==Button(label="O"):
-      turn+="O"
-    elif button4!=Button(label="O")&button4!=Button(label="X"):
-      turn+="-"
-    if button5==Button(label="X"):
-      turn+="X"
-    elif button5==Button(label="O"):
-      turn+="O"
-    elif button5!=Button(label="O")&button5!=Button(label="X"):
-      turn+="-"
-    if button6==Button(label="X"):
-      turn+="X"
-    elif button6==Button(label="O"):
-      turn+="O"
-    elif button6!=Button(label="O")&button6!=Button(label="X"):
-      turn+="-"
-    if button7==Button(label="X"):
-      turn+="X"
-    elif button7==Button(label="O"):
-      turn+="O"
-    elif button7!=Button(label="O")&button7!=Button(label="X"):
-      turn+="-"
-    if button8==Button(label="X"):
-      turn+="X"
-    elif button8==Button(label="O"):
-      turn+="O"
-    elif button8!=Button(label="O")&button8!=Button(label="X"):
-      turn+="-"
-    if button9==Button(label="X"):
-      turn+="X"
-    elif button9==Button(label="O"):
-      turn+="O"
-    elif button9!=Button(label="O")&button9!=Button(label="X"):
-      turn+="-"
+   
+    # if buttons[int(id)]==Button(label="X"):      tried to do the code above right here but it didn't work.
+    #   count=+1
+    #   turn+="X"
+    #   print(count)
+    # elif button1==Button(label="O"):
+    #   turn+="O"
+    # elif button1!=Button(label="O")&button1!=Button(label="X"):
+    #   turn+="-"
+    # if button2==Button(label="X"):
+    #   turn+="X"
+    # elif button2==Button(label="O"):
+    #   turn+="O"
+    # elif button2!=Button(label="O")&button2!=Button(label="X"):
+    #   turn+="-"
+    # if button3==Button(label="X"):
+    #   turn+="X"
+    # elif button3==Button(label="O"):
+    #   turn+="O"
+    # elif button3!=Button(label="O")&button3!=Button(label="X"):
+    #   turn+="-"
+    # if button4==Button(label="X"):
+    #   turn+="X"
+    # elif button4==Button(label="O"):
+    #   turn+="O"
+    # elif button4!=Button(label="O")&button4!=Button(label="X"):
+    #   turn+="-"
+    # if button5==Button(label="X"):
+    #   turn+="X"
+    # elif button5==Button(label="O"):
+    #   turn+="O"
+    # elif button5!=Button(label="O")&button5!=Button(label="X"):
+    #   turn+="-"
+    # if button6==Button(label="X"):
+    #   turn+="X"
+    # elif button6==Button(label="O"):
+    #   turn+="O"
+    # elif button6!=Button(label="O")&button6!=Button(label="X"):
+    #   turn+="-"
+    # if button7==Button(label="X"):
+    #   turn+="X"
+    # elif button7==Button(label="O"):
+    #   turn+="O"
+    # elif button7!=Button(label="O")&button7!=Button(label="X"):
+    #   turn+="-"
+    # if button8==Button(label="X"):
+    #   turn+="X"
+    # elif button8==Button(label="O"):
+    #   turn+="O"
+    # elif button8!=Button(label="O")&button8!=Button(label="X"):
+    #   turn+="-"
+    # if button9==Button(label="X"):
+    #   turn+="X"
+    # elif button9==Button(label="O"):
+    #   turn+="O"
+    # elif button9!=Button(label="O")&button9!=Button(label="X"):
+    #   turn+="-"
 
 
 
@@ -146,19 +217,29 @@ async def TTT(ctx):
 
     print(response)
 
+
+ 
   async def button1Clicked(interaction):
     global id
+    nonlocal rowforbutton
+    global button1
+    rowforbutton=0
     id=interaction.data["custom_id"]
     print(id)
     await interaction.response.send_message("thanks ")
+    button1.disabled=True
     await buttonClick()
+    
     if count%2!=0:
       print("F")
     else:
       print("E")
+   
  
   async def button2Clicked(interaction):
     global id
+    nonlocal rowforbutton
+    rowforbutton=0
     id=interaction.data["custom_id"]
     print(id)
     await interaction.response.send_message("thanks ")
@@ -170,6 +251,8 @@ async def TTT(ctx):
    
   async def button3Clicked(interaction):
     global id
+    nonlocal rowforbutton
+    rowforbutton=0
     id=interaction.data["custom_id"]
     print(id)
     await interaction.response.send_message("thanks ")
@@ -181,6 +264,8 @@ async def TTT(ctx):
 
   async def button4Clicked(interaction):
     global id
+    nonlocal rowforbutton
+    rowforbutton=1
     id=interaction.data["custom_id"]
     print(id)
     await interaction.response.send_message("thanks ")
@@ -192,8 +277,10 @@ async def TTT(ctx):
 
   async def button5Clicked(interaction):
     global id
+    nonlocal rowforbutton
+    rowforbutton=1
     id=interaction.data["custom_id"]
-    print(id)
+    print("rowforbutton")
     await interaction.response.send_message("thanks ")
     await buttonClick()
     if count%2!=0:
@@ -203,6 +290,8 @@ async def TTT(ctx):
 
   async def button6Clicked(interaction):
     global id
+    nonlocal rowforbutton
+    rowforbutton=1
     id=interaction.data["custom_id"]
     print(id)
     await interaction.response.send_message("thanks ")
@@ -214,6 +303,8 @@ async def TTT(ctx):
 
   async def button7Clicked(interaction):
     global id
+    nonlocal rowforbutton
+    rowforbutton=2
     id=interaction.data["custom_id"]
     print(id)
     await interaction.response.send_message("thanks ")
@@ -225,8 +316,10 @@ async def TTT(ctx):
 
   async def button8Clicked(interaction):
     global id
+    nonlocal rowforbutton
+    rowforbutton=2
     id=interaction.data["custom_id"]
-    print(id)
+    print("id")
     await interaction.response.send_message("thanks ")
     await buttonClick()
     if count%2!=0:
@@ -236,6 +329,8 @@ async def TTT(ctx):
 
   async def button9Clicked(interaction):
     global id
+    nonlocal rowforbutton
+    rowforbutton=2
     id=interaction.data["custom_id"]
     print(id)
     await interaction.response.send_message("thanks ")
@@ -244,29 +339,20 @@ async def TTT(ctx):
       print("F")
     else:
       print("E")    
-     
-    #figured out how to change labels once clicked on a button
-    #still need to use the input from the buttonClick() function to somehow use the api to get the best move
-     
-   
-   
    
  
   button1.callback=button1Clicked
-  button2.callback=button1Clicked
-  button3.callback=button1Clicked
-  button4.callback=button1Clicked
-  button5.callback=button1Clicked
-  button6.callback=button1Clicked
-  button7.callback=button1Clicked
-  button8.callback=button1Clicked
-  button9.callback=button1Clicked
+  button2.callback=button2Clicked
+  button3.callback=button3Clicked
+  button4.callback=button4Clicked
+  button5.callback=button5Clicked
+  button6.callback=button6Clicked
+  button7.callback=button7Clicked
+  button8.callback=button8Clicked
+  button9.callback=button9Clicked
   view=View()
  
   view.add_item(button1)  
-  view.add_item(button2)
-  view.add_item(button3)
-  view.add_item(button4)
   view.add_item(button5)
   view.add_item(button6)
   view.add_item(button7)
@@ -276,7 +362,7 @@ async def TTT(ctx):
  
  
 
-  
+ 
  
   await ctx.send("hello",view=view)
 
